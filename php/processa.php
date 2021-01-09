5b2159594dc271e85eba187f16384c873d2ebc86
Fenando ML <vet.fml@gmail.com>
2021-01-09 10:06:28 -0300

    upload

---
+++ php/processa.php
@@ -0,0 +1,15 @@
+<?php
+    $nome= $_POST['nome'];
+	$email= $_POST['email'];
+	$cidade= $_POST['cidade'];
+	$Arquivo= $_POST['Arquivo'];
+	$comodo= $_POST['comodo'];
+	$comentario= $_POST['comentario'];
+
+    echo $nome."<br>";
+	echo $email."<br>";
+	echo $cidade."<br>";
+	echo $Arquivo."<br>";
+	echo $comodo."<br>";
+	echo $comentario;
+?>
